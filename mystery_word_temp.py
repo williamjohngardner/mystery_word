f38aad28249695f1c9cfe82ea28d8a54167347c6
williamjohngardner <bill@williamjohngardner.com>
2016-05-19 18:50:58 -0400

    Late Evening Optomized Commit

---
+++ mystery_word_temp.py
@@ -0,0 +1,37 @@
+import random
+
+with open("words") as opened_file:
+    words = opened_file.read().lower().splitlines()
+
+random_word = random.choice(words).upper()
+print(random_word)
+
+print("Your word contains " + str(len(random_word)) + " letters")
+
+for spaces in random_word:
+        print("# ", end='')
+
+random_word_list = list(random_word)
+
+guess = input("\nPlease enter your guess: ").upper()
+
+good_guesses = []
+bad_guesses = []
+
+while True:
+    for guess in random_word_list:
+        if guess not in good_guesses:
+            good_guesses.append(guess)
+        elif guess not in bad_guesses:
+            bad_guesses.append(guess)
+
+        if guess in good_guesses:
+            print(guess, end='')
+        else:
+            print('_', end='')
+
+    print("\n Good Guesses:" + str(good_guesses))
+    print("\n Bad Guesses: " + str(bad_guesses))
+    guess = input("\nPlease enter your guess: ").upper()
+
+
