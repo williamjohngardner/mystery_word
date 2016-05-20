727f116b9b44e29b9fe8b474eae0248cedc63f6a
williamjohngardner <bill@williamjohngardner.com>
2016-05-20 11:15:23 -0400

    11:15 commit

--- mystery_word_temp.py
+++ mystery_word_temp.py
@@ -4,23 +4,23 @@ with open("words") as opened_file:
     words = opened_file.read().lower().splitlines()
 
 random_word = random.choice(words).upper()
-random_word_list = list(random_word)
+random_letter_list = list(random_word)
 good_guesses = []
 bad_guesses = []
 count = 0
-dash_counter = len(random_word)
+letter_counter = len(random_word)
 
 print(random_word)
-print(dash_counter)
-print("Your word contains " + str(len(random_word_list)) + " letters")
+print(letter_counter)
+print("Your word contains " + str(len(random_letter_list)) + " letters")
 for spaces in random_word:
         print("# ", end='')
 
 guess = input("\nPlease enter your guess: ").upper()
 
-while len(bad_guesses) < 8:
-    for letter in random_word_list:
-        if guess in random_word_list:
+while len(bad_guesses) < 7:
+    for letter in random_letter_list:
+        if guess in random_letter_list:
             if guess not in good_guesses:
                 good_guesses.append(guess)
         else:
@@ -31,11 +31,12 @@ while len(bad_guesses) < 8:
         else:
             print('_ ', end='')
             count += 1
-    dash_counter -= 1
-    print(dash_counter)
-    if dash_counter == 0:
+    print(letter_counter)
+
+    if letter_counter == good_guesses:
         print("You Win!")
         exit()
+
     print("\n Good Guesses:" + str(good_guesses))
     print("\n Bad Guesses: " + str(bad_guesses))
     guess = input("\nPlease enter your guess: ").upper()
