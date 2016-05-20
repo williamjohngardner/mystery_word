87f33513e06d33821e6a2257ab1e4c7f07afef7a
williamjohngardner <bill@williamjohngardner.com>
2016-05-20 15:37:36 -0400

    Thirsty

--- mystery_word_final.py
+++ mystery_word_final.py
@@ -11,6 +11,7 @@ random_word = random.choice(words).upper()
 random_letter_list = list(random_word)
 guesses = ''
 badguesses = ''
+goodguesses = ''
 turns = 8
 
 # print(random_word)
@@ -18,7 +19,7 @@ print("\n \nYour word contains " + str(len(random_letter_list)) + " letters")
 for spaces in random_word:
         print("_ ", end='')
 
-guess = input("\nPlease guess a letter: ").upper()
+guess = input("\n\nPlease guess a letter: ").upper()
 
 while turns > 0:
     if guess in guesses:
@@ -36,14 +37,16 @@ while turns > 0:
     if count == 0:
         print("You Win!")
         exit()
+    if guess in random_letter_list:
+        goodguesses += guess
     if guess not in random_letter_list:
         badguesses += guess
         turns -= 1
-        print("\nYou have", + turns, 'more guesses')
+        print("\n\nYou have", + turns, 'more guesses')
     if turns == 0:
         print("\nYou've used all 8 guesses. Game Over")
         exit()
 
-    print("\n\nLetters Found: " + str(guesses))
+    print("\n\nLetters Found: " + str(goodguesses))
     print("\nLetters Missed: " + str(badguesses))
-    guess = input("\nPlease guess a letter: ").upper()
+    guess = input("\n\nPlease guess a letter: ").upper()
