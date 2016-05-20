8bc4735925957871eda0c4214d6ffbce07bd93de
williamjohngardner <bill@williamjohngardner.com>
2016-05-20 10:25:15 -0400

    Need to fix 2x letter

--- mystery_word_temp.py
+++ mystery_word_temp.py
@@ -8,8 +8,10 @@ random_word_list = list(random_word)
 good_guesses = []
 bad_guesses = []
 count = 0
+dash_counter = len(random_word)
 
 print(random_word)
+print(dash_counter)
 print("Your word contains " + str(len(random_word_list)) + " letters")
 for spaces in random_word:
         print("# ", end='')
@@ -29,8 +31,13 @@ while len(bad_guesses) < 8:
         else:
             print('_ ', end='')
             count += 1
+    dash_counter -= 1
+    print(dash_counter)
+    if dash_counter == 0:
+        print("You Win!")
+        exit()
     print("\n Good Guesses:" + str(good_guesses))
     print("\n Bad Guesses: " + str(bad_guesses))
     guess = input("\nPlease enter your guess: ").upper()
 else:
-    print("Game Over")
+    print("You've used all 8 guesses. Game Over")
