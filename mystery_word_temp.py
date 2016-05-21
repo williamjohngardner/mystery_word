65203c7b40b7a9f0e4e83cbef0606f3abcf71c46
williamjohngardner <bill@williamjohngardner.com>
2016-05-21 15:09:54 -0400

    Updated Normal Mode

--- mystery_word_temp.py
+++ mystery_word_temp.py
@@ -4,50 +4,54 @@ with open("words") as opened_file:
     words = opened_file.read().lower().splitlines()
 
 random_word = random.choice(words).upper()
-random_letter_list = list(random_word)
-good_guesses = []
-bad_guesses = []
-count = 0
-letter_counter = len(random_word)
 
+print("*******MYSTERY WORD GAME*******" +
+      "\n*******************************" +
+      "\nENTER LETTERS TO GUESS THE WORD")
+
+random_letter_list = list(random_word)
+badguesses = ''
+goodguesses = ''
+turns = 8
 print(random_word)
-print(letter_counter)
-print("Your word contains " + str(len(random_letter_list)) + " letters")
+# print(letter_counter)
+print("\nYour word contains " + str(len(random_letter_list)) + " letters")
 for spaces in random_word:
-        print("# ", end='')
+        print("_ ", end='')
 
 guess = input("\nPlease enter your guess: ").upper()
 guesses = ''
 guesses += guess
 
-while len(bad_guesses) < 7:
-    for letter in random_letter_list:
-        if guess in random_letter_list:
-            if guess not in good_guesses:
-                good_guesses.append(guess)
-        else:
-            if guess not in bad_guesses:
-                bad_guesses.append(guess)
-        if letter in good_guesses:
-            print(letter, end='')
-        else:
-            print('_ ', end='')
-            count += 1
-    print(count)
+while turns > 0:
+    if guess in guesses:
+        print("\nYou've already guessed that letter.")
+    else:
+        guesses += guess
 
+    count = 0
+    for char in random_letter_list:
+        if char in guesses:
+            print(char, end='')
+        else:
+            print("_ ", end='')
+            count += 1
     if count == 0:
-        print("You Win!")
+        print("\n******************"
+              "\n*****You Win!*****"
+              "\n******************")
+        exit()
+    if guess in random_letter_list:
+        goodguesses += guess
+    if guess not in random_letter_list:
+        badguesses += guess
+        turns -= 1
+    if turns == 0:
+        print("\n\n***You've used all 8 guesses. Game Over***")
+        print("\nYour word was: " + str(random_word))
         exit()
 
-    print("\n Good Guesses:" + str(good_guesses))
-    print("\n Bad Guesses: " + str(bad_guesses))
+    print("\n\nLetters Found: " + str(goodguesses))
+    print("\nLetters Missed: " + str(badguesses))
+    print("\n\nYou have", + turns, 'more guesses')
     guess = input("\nPlease enter your guess: ").upper()
-else:
-    print("You've used all 8 guesses. Game Over")
-
-for char in random_letter_list:
-    if char in guesses:
-        print(char)
-    else:
-        print("_ ")
-        count += 1
