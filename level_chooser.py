31ae425d7e71f1285dd5e35b8c6f1471ded78d2d
williamjohngardner <bill@williamjohngardner.com>
2016-05-22 21:29:55 -0400

    Final Commit for Review

--- level_chooser.py
+++ level_chooser.py
@@ -3,33 +3,34 @@ def level_chooser():
     from random_word_generator import random_word_generator
 
     level_choice = int(input("\nChoose a level: " +
-                         "\nEasy:   1" +
-                         "\nMedium: 2" +
-                         "\nHard:   3" +
-                         "\nEnter your choice:"))
+                             "\nEasy:   1" +
+                             "\nMedium: 2" +
+                             "\nHard:   3" +
+                             "\nEnter your choice:"))
 
-    if level_choice is not 4 < level_choice > 0:
+    if level_choice is not 3 <= level_choice >= 1:
             print("\nPlease only enter a choice between 1 & 3: ")
             level_choice = int(input("\nChoose a level: " +
-                                 "\nEasy:   1" +
-                                 "\nMedium: 2" +
-                                 "\nHard:   3" +
-                                 "\nEnter your choice:"))
+                                     "\nEasy:   1" +
+                                     "\nMedium: 2" +
+                                     "\nHard:   3" +
+                                     "\nEnter your choice:"))
+    random_word = ''
 
     while True:
         random_word = random_word_generator()
         if level_choice == 1:
-            if len(random_word) > 6:
+            if len(random_word) >= 7:
                 continue
             else:
                 break
         if level_choice == 2:
-            if len(random_word) > 11 | len(random_word) < 7:
+            if len(random_word) >= 12 | len(random_word) <= 6:
                 continue
             else:
                 break
         if level_choice == 3:
-            if len(random_word) < 11:
+            if len(random_word) <= 12:
                 continue
             else:
                 break
