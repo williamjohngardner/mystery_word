02de73640e3a7b8802fc85f4e2340a3b14551cf5
williamjohngardner <bill@williamjohngardner.com>
2016-05-22 21:14:29 -0400

    Bug in error check fixed

--- user_input.py
+++ user_input.py
@@ -4,16 +4,17 @@ def user_input():
 
     guess = input("\n\nPlease guess a letter: ").upper()
 
-    for item in string.punctuation:
-        if guess == item:
+    while True:
+        for item in string.punctuation:
+            if guess == item:
+                print("\nEntry must be a letter!")
+                guess = input("\n\nPlease guess a letter: ").upper()
+        if type(guess) == int:
             print("\nEntry must be a letter!")
             guess = input("\n\nPlease guess a letter: ").upper()
-
-    if type(guess) == int:
-        print("\nEntry must be a letter!")
-        guess = input("\n\nPlease guess a letter: ").upper()
-    elif len(guess) > 1:
-        print("\nPlease enter only one letter at a time!")
-        guess = input("\n\nPlease guess a letter: ").upper()
-    else:
-        return guess
+        elif len(guess) > 1:
+            print("\nPlease enter only one letter at a time!")
+            guess = input("\n\nPlease guess a letter: ").upper()
+            continue
+        else:
+            return guess
